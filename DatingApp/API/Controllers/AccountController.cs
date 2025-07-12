aa7a7bfe2a94d06e63492ff11ebde7e82435fe46
Abhishek Kumar <akkr0077@gmail.com>
2025-07-12 12:29:50 +0530

    Added code changes for basic authentication and new controllers

---
+++ DatingApp/API/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using API.Data;
+using API.DTOs;
+using System.Text;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(DataContext context) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        {
+            if (await EmailExist(registerDto.Email)) return BadRequest("Email already Taken");
+
+            var hmac = new HMACSHA512();
+            var user = new AppUser
+            {
+                Email = registerDto.Email,
+                UserName = registerDto.DisplayName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return Ok(user);
+
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(x => x.Email == loginDto.Email);
+            if (user is null) return Unauthorized("Invalid Email");
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+            for (var i = 0; i < computeHash.Length; i++)
+            {
+                if (computeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
+            return Ok(user);
+        }
+
+        private async Task<bool> EmailExist(string email)
+        {
+            return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+        }
+
+    }
+}
